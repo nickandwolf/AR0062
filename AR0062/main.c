c1cc569fde2ef417b6eaeb2126e040221ede8403
Nicholas Nelson <nickandwolf@gmail.com>
2019-08-17 15:18:37 +0900

    Initial Commit

---
+++ AR0062/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AR0062
+//
+//  Created by Nicholas A Nelson on 8/17/19.
+//  Copyright © 2019 Nick & Wolf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
